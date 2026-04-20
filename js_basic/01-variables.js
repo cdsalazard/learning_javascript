030f695721131ece22a2c185fa010b4b9315e28f
cdsalazard <cdsalazard@unal.edu.co>
2026-04-19 19:25:19 -0500

    Uso de ver, let, const

---
+++ js_basic/01-variables.js
@@ -0,0 +1,25 @@
+// Crear variables: var, let, const
+
+
+// var
+var helloWorld = "Hello, JavaScript"
+console.log(helloWorld)
+
+helloWorld = "Hello new Var"
+console.log(helloWorld)
+
+// let: las variables let solo son accesibles dentro de un bloque
+let helloWorld2 = "Hello, JavaScript con let"
+console.log(helloWorld2)
+
+helloWorld2 = "Hello, JavaScript con let, reasignada"
+console.log(helloWorld2)
+
+
+// const: las variables let solo son accesibles dentro de un bloque
+const helloWorld3 = "Hello, JavaScript con const"
+console.log(helloWorld3)
+
+// Esto no se puede hacer
+// helloWorld3 = "Reasignar valor a const"
+// console.log(helloWorld3)
