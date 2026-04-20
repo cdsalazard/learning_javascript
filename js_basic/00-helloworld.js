dfa5ba8a5f5aeaace264c832d82146f0f7be4ac1
cdsalazard <cdsalazard@unal.edu.co>
2026-04-19 19:15:40 -0500

    Operaciones matematicas basicas agregadas

---
+++ js_basic/00-helloworld.js
@@ -0,0 +1,13 @@
+// Console interactua con la terminal
+console.log("Hola mundo javascript")
+console.log("Hola mundo, javascript")
+
+// Operaciones numericas
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 / 2)
+console.log(5 * 2)
+console.log(5 % 2)
+console.log(5 **2)
+
