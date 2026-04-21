1f4a13680875e489348ec34ae84434cb8ff1a0d0
cdsalazard <cdsalazard@unal.edu.co>
2026-04-20 22:26:55 -0500

    se agrega operador ternario

---
+++ 08-condiciones.js
@@ -0,0 +1,18 @@
+// if, else if, else
+
+
+let age = 10
+// if
+if(age == 37){
+    console.log("Tu edad es 37")
+} else if (age < 18){
+    console.log("Eres menor de edad")
+} else  {
+    console.log("Tu edad no es 37")
+}
+
+
+// Operador ternario
+
+const message = age == 27 ? "La edad es 27" : "La edad no es 37"
+console.log(message)
